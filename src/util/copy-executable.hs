c615b83d5d06e113d9e607cd69a866bbfab7f064
Taylor Fausak <taylor@fausak.me>
2020-11-28 19:59:20 +0000

    Try to fix path on Windows

--- src/util/copy-executable.hs
+++ src/util/copy-executable.hs
@@ -7,6 +7,11 @@ import qualified System.Process as Process
 main :: IO ()
 main = do
   raw <- Process.readProcess "cabal" [ "exec", "which", "zippy" ] ""
-  let path = List.dropWhileEnd Char.isSpace raw
+  let path = convert $ List.dropWhileEnd Char.isSpace raw
   print path
   Directory.copyFile path "output/zippy"
+
+convert :: FilePath -> FilePath
+convert path = case path of
+  '/' : drive : '/' : rest -> drive : ':' : '/' : rest
+  _ -> path
